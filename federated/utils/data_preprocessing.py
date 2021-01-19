ed0adf26178d996602d30c5227b02ccee66bf55c
Pernille Kopperud <pernilko@stud.ntnu.no>
2021-01-19 18:59:33 +0100

    conversion to tff

--- federated/utils/data_preprocessing.py
+++ federated/utils/data_preprocessing.py
@@ -1,6 +1,16 @@
 import tensorflow as tf
 import pandas as pd
 import tensorflow_federated as tff
+import numpy as np
+import collections
+from matplotlib import pyplot as plt
+
+
+def _preprocess_dataframe(df):
+    df[187] = df[187].astype(int)
+    target = df[df.columns[-1]]
+    df.drop(df.columns[-1], axis=1, inplace=True)
+    return df, target
 
 
 def load_data():
@@ -13,21 +23,30 @@ def load_data():
         test_df.astype(int),
     )
 
-    client_ids = train_df[187].unique()
+    train_X, train_y = _preprocess_dataframe(train_df)
+    test_X, test_y = _preprocess_dataframe(test_df)
 
-    def create_tf_dataset_for_client(client_id, df):
-        client_data = df[df[187] == client_id]
-        dataset = tf.data.Dataset.from_tensor_slices(client_data.to_dict("list"))
-        return dataset
+    num_of_clients = len(train_y)
+    total_img_count = len(train_X)
+    image_per_set = int(np.floor(total_img_count / num_of_clients))
 
-    train_data = tff.simulation.ClientData.from_clients_and_fn(
-        client_ids=train_client_ids,
-        create_tf_dataset_for_client_fn=create_tf_dataset_for_client_fn,
-    )
-    test_data = tff.simulation.ClientData.from_clients_and_fn(
-        client_ids=test_client_ids,
-        create_tf_dataset_for_client_fn=create_tf_dataset_for_client_fn,
-    )
+    client_train_dataset = collections.OrderedDict()
+    for i in range(1, num_of_clients + 1):
+        name_of_client = "client_" + str(i)
+        start = image_per_set * (i - 1)
+        end = image_per_set * i
+
+        data = collections.OrderedDict(
+            (("label", train_y[start:end]), ("pixels", train_X[start:end]))
+        )
+        client_train_dataset[name_of_client] = data
+
+    train_dataset = tff.simulation.FromTensorSlicesClientData(client_train_dataset)
+    samples = train_dataset.create_tf_dataset_for_client(train_dataset.client_ids[0])
+    element = next(iter(samples))
+
+    print(element)
+    print(samples.element_spec)
 
 
 if __name__ == "__main__":
