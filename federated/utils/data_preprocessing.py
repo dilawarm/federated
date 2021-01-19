23aeb754fa546eef3bea284539f869b8e5302d62
Dilawar Mahmood <dilawarmm@outlook.com>
2021-01-19 17:23:28 +0100

    fixed data processing

--- federated/utils/data_preprocessing.py
+++ federated/utils/data_preprocessing.py
@@ -3,22 +3,16 @@ import pandas as pd
 import tensorflow_federated as tff
 
 
-def _preprocess_dataframe(df):
-    df[187] = df[187].astype(int)
-    target = df[df.columns[-1]]
-    df.drop(df.columns[-1], axis=1, inplace=True)
-    return df, target
-
-
 def load_data():
     """"""
-    mitbih_train_df = pd.read_csv("../../data/mitbih/mitbih_train.csv", header=None)
-    mitbih_test_df = pd.read_csv("../../data/mitbih/mitbih_test.csv", header=None)
+    train_df = pd.read_csv("../../data/mitbih/mitbih_train.csv", header=None)
+    test_df = pd.read_csv("../../data/mitbih/mitbih_test.csv", header=None)
 
-    train_X, train_y = _preprocess_dataframe(mitbih_train_df)
-    test_X, test_y = _preprocess_dataframe(mitbih_test_df)
-
-    mitbih_train, mitbih_test = (
-        tf.data.Dataset.from_tensor_slices(()),
-        tf.data.Dataset.from_tensor_slices(()),
+    train_df[187], mitbih_test_df = (
+        train_df[187].astype(int),
+        test_df.astype(int),
     )
+
+
+if __name__ == "__main__":
+    load_data()
