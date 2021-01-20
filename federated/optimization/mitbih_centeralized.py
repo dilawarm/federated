8134d24773ab552f95a6bf4366a1246762656175
Dilawar Mahmood <dilawarmm@outlook.com>
2021-01-20 14:09:26 +0100

    fix

--- federated/optimization/mitbih_centeralized.py
+++
@@ -1,2 +0,0 @@
-import tensorflow as tf
-from federated.utils import centralized_training_loop
---
+++ federated/optimization/mitbih_centralized.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+from federated.utils.training_loops import centralized_training_loop
+
