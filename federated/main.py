babf01c821b2249a0c0d5f4a953bae7474443daa
Pernille Kopperud <pernilko@stud.ntnu.no>
2021-01-20 15:36:26 +0100

    made centralized pipeline

---
+++ federated/main.py
@@ -0,0 +1,7 @@
+from federated.optimization.mitbih_centralized import centralized_pipeline
+
+
+def run_centralized():
+    centralized_pipeline(
+        "centralized_exp_1",
+    )
--- federated/optimization/mitbih_centralized.py
+++ federated/optimization/mitbih_centralized.py
@@ -1,2 +1,31 @@
 import tensorflow as tf
 from federated.utils.training_loops import centralized_training_loop
+from federated.utils.mitbih_data_preprocessing import get_centralized_datasets
+from federated.models.mitbih_model import create_cnn_model
+
+
+def centralized_pipeline(name, output, epochs, batch_size, optimizer):
+    """
+    Function runs centralized training pipeline
+    """
+    train_dataset, test_dataset = get_centralized_datasets(
+        train_batch_size=batch_size,
+    )
+
+    model = create_cnn_model()
+
+    model.compile(
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        optimizer=optimizer,
+        metrics=[tf.keras.metrics.Accuracy()],
+    )
+
+    centralized_training_loop(
+        model,
+        train_dataset,
+        name,
+        epochs,
+        output,
+        save_model=True,
+        validation_dataset=test_dataset,
+    )
