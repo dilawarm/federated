a3b73885cb302a5721ce57427637613fa2da251f
Dilawar Mahmood <dilawarmm@outlook.com>
2021-01-20 13:56:02 +0100

    added model tests

--- federated/utils/training_loops_test.py
+++ federated/utils/training_loops_test.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from training_loops import centralized_training_loop
+import os
 
 def create_test_dataset():
     dataset = tf.data.Dataset.from_tensor_slices(
@@ -34,6 +35,20 @@ class TrainingLoopsTest(tf.test.TestCase):
         history = centralized_training_loop(model, dataset, "test_reduces_loss", epochs=5, output=self.get_temp_dir(), validation_dataset=dataset)
         for metric in ["loss", "mean_squared_error", "val_loss", "val_mean_squared_error"]:
             self.assertLess(history.history[metric][-1], history.history[metric][0])
+
+    def test_write_metrics(self):
+        dataset = create_test_dataset()
+        model = create_test_model()
+        name = "test_write_metrics"
+        output = self.get_temp_dir()
+
+        history = centralized_training_loop(model, dataset, name, epochs=1, output=output, validation_dataset=dataset)
+
+        log_dir = os.path.join(output, "logdir", name)
+        train_log_dir = os.path.join(log_dir, "train")
+        val_log_dir = os.path.join(log_dir, "validation")
+        for gfile in [output, log_dir, train_log_dir, val_log_dir]:
+            self.assertTrue(tf.io.gfile.exists(gfile))
 
 if __name__ == "__main__":
     tf.test.main()
