3cd949891f1c880ffe67baf453222af7049c191f
Pernille Kopperud <pernilko@stud.ntnu.no>
2021-01-25 09:56:39 +0100

    fix test

--- federated/utils/mitbih_data_preprocessing_test.py
+++ federated/utils/mitbih_data_preprocessing_test.py
@@ -31,12 +31,12 @@ class DataPreprocessorTest(tf.test.TestCase):
         ds_test = test.create_tf_dataset_for_client(train.client_ids[0])
 
         for train_batch in ds_train:
-            train_batch_shape = np.array(train_batch[:]).shape
+            train_batch_shape = train_batch[0].shape[1:]
         for test_batch in ds_test:
-            test_batch_shape = np.array(test_batch[:]).shape
+            test_batch_shape = test_batch[0].shape[1:]
 
-        self.assertEqual(train_batch_shape, [32, 186, 1])
-        self.assertEqual(test_batch_shape, [100, 186, 1])
+        self.assertEqual(train_batch_shape, [186, 1])
+        self.assertEqual(test_batch_shape, [186, 1])
 
 
 if __name__ == "__main__":
