735904b2fff936b7ff63000f5f31a554bcc0047e
Pernille Kopperud <pernilko@stud.ntnu.no>
2021-01-25 09:10:36 +0100

    Added data preprocessing test for federated

--- federated/utils/mitbih_data_preprocessing_test.py
+++ federated/utils/mitbih_data_preprocessing_test.py
@@ -3,12 +3,27 @@ from federated.utils.mitbih_data_preprocessing import get_datasets
 
 
 class DataPreprocessorTest(tf.test.TestCase):
-    def test_dataset_shapes(self):
+    def test_dataset_shapes_centralized(self):
         """
         Function that tests function for centeralized data preprocessing.
         """
         train, test = get_datasets(
             train_batch_size=32, test_batch_size=100, centralized=True
+        )
+
+        train_batch = next(iter(train))
+        train_batch_shape = train_batch[0].shape
+        test_batch = next(iter(test))
+        test_batch_shape = test_batch[0].shape
+        self.assertEqual(train_batch_shape, [32, 186, 1])
+        self.assertEqual(test_batch_shape, [100, 186, 1])
+
+    def test_dataset_shapes_federated(self):
+        """
+        Function that tests function for federated data preprocessing.
+        """
+        train, test = get_datasets(
+            train_batch_size=32, test_batch_size=100, centralized=False
         )
 
         train_batch = next(iter(train))
