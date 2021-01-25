ec65858dd82c6d85eaf20b26ac1a7540a8bef827
Pernille Kopperud <pernilko@stud.ntnu.no>
2021-01-25 09:49:16 +0100

    fixing tests

--- federated/utils/mitbih_data_preprocessing_test.py
+++ federated/utils/mitbih_data_preprocessing_test.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from federated.utils.mitbih_data_preprocessing import get_datasets
+import numpy as np
 
 
 class DataPreprocessorTest(tf.test.TestCase):
@@ -27,10 +28,13 @@ class DataPreprocessorTest(tf.test.TestCase):
         )
 
         ds_train = train.create_tf_dataset_for_client(train.client_ids[0])
+        ds_test = test.create_tf_dataset_for_client(train.client_ids[0])
 
-        train_batch_shape = ds_train[0].shape
-        ds_test = test.create_tf_dataset_for_client(test.client_ids[0])
-        test_batch_shape = ds_test[0].shape
+        for train_batch in ds_train:
+            train_batch_shape = np.array(train_batch[:]).shape
+        for test_batch in ds_test:
+            test_batch_shape = np.array(test_batch[:]).shape
+
         self.assertEqual(train_batch_shape, [32, 186, 1])
         self.assertEqual(test_batch_shape, [100, 186, 1])
 
