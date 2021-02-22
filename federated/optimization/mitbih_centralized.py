63586fc984198c0e381223f038616fd291785c3f
Dilawar Mahmood <dilawarmm@outlook.com>
2021-02-22 13:48:45 +0100

    centralized

--- federated/optimization/mitbih_centralized.py
+++ federated/optimization/mitbih_centralized.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 from federated.utils.training_loops import centralized_training_loop
 from federated.data.mitbih_data_preprocessing import get_datasets
-from federated.models.mitbih_model import create_cnn_model, create_dense_model
+from federated.models.mitbih_model import (
+    create_cnn_model,
+    create_dense_model,
+    create_new_cnn_model,
+)
 
 
 def centralized_pipeline(
@@ -20,7 +24,7 @@ def centralized_pipeline(
         train_batch_size=batch_size, transform=False, centralized=True
     )
 
-    model = create_dense_model()
+    model = create_new_cnn_model()
 
     model.compile(
         loss="categorical_crossentropy",
