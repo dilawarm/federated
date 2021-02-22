a58688951fbe82e18ce66026deb5e1543b1fe6ea
Dilawar Mahmood <dilawarmm@outlook.com>
2021-02-22 11:31:41 +0100

    test

--- federated/optimization/mitbih_federated.py
+++ federated/optimization/mitbih_federated.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     federated_pipeline(
         name=name,
         iterative_process_fn=iterative_process_fn,
-        server_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=0.001),
+        server_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=1.0),
         data_selector=create_uniform_dataset,
         output="history",
         client_epochs=10,
@@ -206,7 +206,7 @@ if __name__ == "__main__":
         keras_model_fn=create_new_cnn_model,
         normalized=True,
         save_data=False,
-        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.02),
+        client_optimizer_fn=lambda: tf.keras.optimizers.Adam(learning_rate=0.02),
         aggregation_method=aggregation_method,
         iterations=2,
         v=1e-6,
